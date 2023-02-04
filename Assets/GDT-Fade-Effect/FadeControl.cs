a0b2df7d4c3675c36c185db01328401e1c65cc85
Funbam <bambamtheram@gmail.com>
2023-02-04 08:20:18 -0800

    Fade infrastructure in

---
+++ Assets/GDT-Fade-Effect/FadeControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeControl : MonoBehaviour
+{
+
+    public GameObject fadeEffect;
+
+    public void StartFade()
+    {
+        Debug.Log("REEE");
+        fadeEffect.SetActive(true);
+    }
+}

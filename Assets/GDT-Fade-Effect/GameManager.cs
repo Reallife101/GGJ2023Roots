7050ddf0ee9b2dcb55e5ca525625df5ac73e4ce9
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-02-04 17:32:54 -0800

    add amogus transition

--- Assets/GDT-Fade-Effect/GameManager.cs
+++ Assets/GDT-Fade-Effect/GameManager.cs
@@ -24,7 +24,13 @@ public class GameManager : MonoBehaviour
     public GameObject menu;
     public GameObject levelSelect;
     public LevelSelect levelSelector;
-    public static GameManager gameManager; 
+    public static GameManager gameManager;
+
+    [SerializeField]
+    AudioClip levelTransition;
+
+    [SerializeField]
+    AudioSource au;
 
     private void Awake()
     {
@@ -125,6 +131,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator LoadLevel(string sceneToLoad)
     {
+        au.PlayOneShot(levelTransition, 1f);
         levelEndFade.SetActive(true);
         yield return new WaitForSeconds(levelEndFade.GetComponent<GDTFadeEffect>().timeEffect);
         SceneManager.LoadScene(sceneToLoad);

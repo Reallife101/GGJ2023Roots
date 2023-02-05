fe937c3e228211c399ec232257f5e8b37e8dde50
Reallife101 <ryan.ngoon@gmail.com>
2023-02-05 12:07:26 -0800

    add levels

---
+++ Assets/loadgoodorbad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadgoodorbad : MonoBehaviour
+{
+    public float deathCount;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (UIManager.UImanager.deathCount > deathCount)
+        {
+
+        }
+    }
+}
